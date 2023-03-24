4f665530687c2a7e61786b4631057a78aa1203ce
Dmitry 'demqa' Stepanov <greatsteamguard@gmail.com>
2023-03-25 00:21:43 +0300

    Dump function bodies in dot.

--- DemqaPass/DemqaPass.cpp
+++ DemqaPass/DemqaPass.cpp
@@ -1,29 +1,74 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include <ios>
+#include <llvm/ADT/StringRef.h>
+#include <ostream>
+#include <fstream>
+#include <system_error>
 
 using namespace llvm;
 
+const char dumpFilename[] = "dump";
+
 namespace {
   struct DemqaPass : public FunctionPass {
+private:
+    std::error_code error_code;
+    llvm::raw_fd_ostream dumpFile{dumpFilename, error_code};
+
+public:
     static char ID;
-    DemqaPass() : FunctionPass(ID) {}
+    DemqaPass() : FunctionPass(ID) {
+        dumpFile << "digraph structs {\n";
+        dumpFile << "node [color=black, shape=box, style=\"filled\"]\n";
+        dumpFile << "rankdir=UD;\n";
+    }
+
+   ~DemqaPass() {
+       dumpFile << "}\n";
+       dumpFile.close();
+
+       system((std::string("dot ") + dumpFilename + " -T png -o " + dumpFilename + ".png").c_str());
+       system((std::string("gwenview ") + dumpFilename + ".png").c_str());
+    }
 
     bool runOnFunction(Function &F) override {
+      static int launch = 0;
 
-      errs() << "\n"
-                "------------FUNCTION START------------\n";
-      errs() << "NAME: " << F.getName() << '\n';
-      errs() << "------------FUNCTION BOODY------------\n\n";
+      dumpFile << "func"; dumpFile.write_hex(size_t(&F)); dumpFile << " ";
+      dumpFile << "[fillcolor=\"white\"," << "label=\"\n";
+      F.print(dumpFile);
+      dumpFile << "\"" << "];\n";
 
-      for (auto& B: F) {
-          errs() << B.getName() << '\n';
-          for (auto& I: B) {
-              errs() << I << '\n';
+      // Dump function uses
+      for (auto &U : F.uses()) {
+          User *user = U.getUser();
+          // outs() << "[DOT] " << (uint64_t)(&F) << " -> " << (uint64_t)user << "\n";
+          outs() << "User:  " << (uint64_t)user;
+          user->print(llvm::outs(), true);
+          outs() << "\n";
+      }
+
+      for (auto &B : F) {
+          for (auto &I : B) {
+              // Dump Instructions
+              outs() << "Instruction: " << (uint64_t)(&I) << "\n";
+              I.print(outs(), true);
+              outs() << "\n";
+
+              // Dump instruction uses
+              for (auto &U : I.uses()) {
+                  User *user = U.getUser();
+                  // outs() << "[DOT] " << (uint64_t)(&I) << " -> " << (uint64_t)user << "\n     ";
+                  outs() << "  User:  " << (uint64_t)user << "\n";
+                  // user->print(outs(), true);
+                  // outs() << "\n";
+              }
           }
       }
 
-      errs() << "\n"
+      outs() << "\n"
                 "-------------FUNCTION END-------------\n";
 
       return false;
--- run_pass.sh
+++ run_pass.sh
@@ -5,5 +5,5 @@ name="${1%.*}"
 
 ninja
 ./bin/clang -c -emit-llvm $1
-./bin/opt -enable-new-pm=0 -load lib/LLVMOurPass.so -demqa < $name.bc > /dev/null
+./bin/opt -enable-new-pm=0 -load lib/LLVMOurPass.so -demqa < $name.bc
 
